f69ae01bea8d3b99ea9e4e4746ec3f6c3d060ad0
Tiger <thisisstreamliner@gmail.com>
2021-03-10 15:35:32 +0200

    Setting up Firebase Packages for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
+import 'dart:io';
+
 import 'package:cab_driver/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    final FirebaseApp app = await Firebase.initializeApp(
+      name: 'db2',
+      options: Platform.isIOS || Platform.isMacOS
+          ? FirebaseOptions(
+              appId: '1:993836699208:ios:21daffc4cf81311cfb527d',
+              apiKey: 'AIzaSyAXw7GcRdCoQE7S1yEF1aplSAiR2mIJPGs',
+              projectId: 'uberclone-983d8',
+              messagingSenderId: '993836699208',
+              databaseURL:
+                  'https://uberclone-983d8-default-rtdb.firebaseio.com',
+            )
+          : FirebaseOptions(
+              appId: '1:993836699208:android:9b7b1441a9978f16fb527d',
+              apiKey: 'AIzaSyAXw7GcRdCoQE7S1yEF1aplSAiR2mIJPGs',
+              messagingSenderId: '993836699208',
+              projectId: 'uberclone-983d8',
+              databaseURL:
+                  'https://uberclone-983d8-default-rtdb.firebaseio.com',
+            ),
+    );
+  } catch (e) {
+    print('try app catch $e');
+  }
   runApp(MyApp());
 }
 

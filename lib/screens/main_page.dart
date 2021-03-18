bc690da7b731c5e1f6707d02ef8b36f2d62cf09f
Tiger <thisisstreamliner@gmail.com>
2021-03-18 13:08:22 +0200

    Setting up TabController and TabPages

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,10 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cab_driver/brand_colors.dart';
+import 'package:cab_driver/screens/tabs/account_tab.dart';
+import 'package:cab_driver/screens/tabs/earnings_tab.dart';
+import 'package:cab_driver/screens/tabs/home_tab.dart';
+import 'package:cab_driver/screens/tabs/rating_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatefulWidget {
   static const String id = "main_page";
@@ -7,24 +12,69 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  int _selectedIndex = 0;
+
+  void _onItemClicked(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _tabController.index = _selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Driver App'),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _tabController,
+        children: [
+          HomeTab(),
+          EarningsTab(),
+          RatingTab(),
+          AccountTab(),
+        ],
       ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            DatabaseReference ref =
-                FirebaseDatabase.instance.reference().child('testing');
-            ref.set('testing connection');
-          },
-          color: Colors.blue,
-          minWidth: 200,
-          child: Text('Test connection'),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: BrandColors.colorIcon,
+        selectedItemColor: BrandColors.colorOrange,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.creditCard),
+            label: 'Earnings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.star),
+            label: 'Rating',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.userAlt),
+            label: 'Account',
+          ),
+        ],
+        onTap: _onItemClicked,
+        currentIndex: _selectedIndex,
       ),
     );
   }

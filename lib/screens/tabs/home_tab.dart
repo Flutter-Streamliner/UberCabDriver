3f2da22163f3d36cef2ee6418160a3ef1ab0539c
Tiger <thisisstreamliner@gmail.com>
2021-03-19 12:28:51 +0200

    Setting up Geolocator Service

--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cab_driver/global_variables.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTab extends StatefulWidget {
@@ -12,6 +13,16 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   GoogleMapController _googleMapController;
   Completer<GoogleMapController> _completer = Completer();
+
+  Future<Position> _getCurrentPosition() async =>
+      await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.bestForNavigation);
+
+  void _setCameraToNewPosition(Position position) {
+    LatLng posLatLng = LatLng(position.latitude, position.longitude);
+    _googleMapController.animateCamera(CameraUpdate.newLatLng(posLatLng));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -20,9 +31,10 @@ class _HomeTabState extends State<HomeTab> {
             myLocationEnabled: true,
             myLocationButtonEnabled: true,
             mapType: MapType.normal,
-            onMapCreated: (GoogleMapController controller) {
+            onMapCreated: (GoogleMapController controller) async {
               _googleMapController = controller;
               _completer.complete(controller);
+              _setCameraToNewPosition(await _getCurrentPosition());
             },
             initialCameraPosition: kGooglePlex),
       ],

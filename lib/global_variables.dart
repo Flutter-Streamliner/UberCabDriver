4c3cb011b8258518df103c64c5683aeb856a4280
Tiger <thisisstreamliner@gmail.com>
2021-03-19 11:55:29 +0200

    Setting up Google Map

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,3 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 User currentFirebaseUser;
+
+const String androidKey = 'AIzaSyAXw7GcRdCoQE7S1yEF1aplSAiR2mIJPGs';
+const String iOsKey = 'AIzaSyCfAG7BfkyrZkusIdRPOoBUPFp56trfW_o';
+
+final CameraPosition kGooglePlex = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,10 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeTab extends StatelessWidget {
+import 'package:cab_driver/global_variables.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeTab extends StatefulWidget {
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  GoogleMapController _googleMapController;
+  Completer<GoogleMapController> _completer = Completer();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home Tab"),
+    return Stack(
+      children: [
+        GoogleMap(
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            mapType: MapType.normal,
+            onMapCreated: (GoogleMapController controller) {
+              _googleMapController = controller;
+              _completer.complete(controller);
+            },
+            initialCameraPosition: kGooglePlex),
+      ],
     );
   }
 }
